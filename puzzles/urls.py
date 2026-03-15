9609ed6afa1b7ced3deea54e8cc4c43ae0fa2b34
deadr1pley <vanmatrix@hotmail.com>
2026-03-15 19:18:03 +0100

    Add edit and delete functionality for puzzles

--- puzzles/urls.py
+++ puzzles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import puzzle_list, add_puzzle
+from .views import puzzle_list, add_puzzle, edit_puzzle, delete_puzzle
 
 urlpatterns = [
     path('', puzzle_list, name='puzzle_list'),
     path('add/', add_puzzle, name='add_puzzle'),
+    path('add/', add_puzzle, name='add_puzzle'),
+    path('edit/<int:puzzle_id>/', edit_puzzle, name='edit_puzzle'),
+    path('delete/<int:puzzle_id>/', delete_puzzle, name='delete_puzzle'),
 ]
--- puzzles/views.py
+++ puzzles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Puzzle
 from .forms import PuzzleForm
@@ -22,3 +22,27 @@ def add_puzzle(request):
         form = PuzzleForm()
 
     return render(request, 'puzzles/add_puzzle.html', {'form': form})
+
+@login_required
+def edit_puzzle(request, puzzle_id):
+    puzzle = get_object_or_404(Puzzle, id=puzzle_id, user=request.user)
+
+    if request.method == 'POST':
+        form = PuzzleForm(request.POST, instance=puzzle)
+        if form.is_valid():
+            form.save()
+            return redirect('puzzle_list')
+    else:
+        form = PuzzleForm(instance=puzzle)
+
+    return render(request, 'puzzles/edit_puzzle.html', {'form': form, 'puzzle': puzzle})
+
+@login_required
+def delete_puzzle(request, puzzle_id):
+    puzzle = get_object_or_404(Puzzle, id=puzzle_id, user=request.user)
+
+    if request.method == 'POST':
+        puzzle.delete()
+        return redirect('puzzle_list')
+
+    return render(request, 'puzzles/delete_puzzle.html', {'puzzle': puzzle})
