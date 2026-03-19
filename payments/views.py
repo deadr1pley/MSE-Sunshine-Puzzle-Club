a16f4a4d72fe58f7931df1ee194c19be4b00c662
deadr1pley <vanmatrix@hotmail.com>
2026-03-19 13:09:42 +0100

    Uppdtaed code in payments views.py so the Stripe would work in the live page in heroku.

--- payments/views.py
+++ payments/views.py
@@ -2,22 +2,27 @@ import stripe
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-# Create your views here.
+from django.urls import reverse
+from accounts.models import UserProfile
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
+
 @login_required
 def premium_page(request):
-    profile = request.user.userprofile
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
     return render(request, 'payments/premium.html', {
         'profile': profile,
         'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
     })
 
+
 @login_required
 def create_checkout_session(request):
     if request.method == 'POST':
+        success_url = request.build_absolute_uri(reverse('payment_success'))
+        cancel_url = request.build_absolute_uri(reverse('premium_page'))
+
         checkout_session = stripe.checkout.Session.create(
             payment_method_types=['card'],
             line_items=[
@@ -33,16 +38,17 @@ def create_checkout_session(request):
                 }
             ],
             mode='payment',
-            success_url='http://127.0.0.1:8000/payments/success/',
-            cancel_url='http://127.0.0.1:8000/payments/premium/',
+            success_url=success_url,
+            cancel_url=cancel_url,
         )
         return redirect(checkout_session.url, code=303)
 
     return redirect('premium_page')
 
+
 @login_required
 def payment_success(request):
-    profile, created = UserProfile.objects.get_or_create(User=request.user)
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
     profile.is_premium = True
     profile.save()
 
